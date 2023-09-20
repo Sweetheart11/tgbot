d1e255d1333266bf57d7e7bf3cfbc53ce4f2222b
Sweetheart11 <yanik20006@gmail.com>
2023-09-20 15:40:34 +0300

    :construction: Installed goose, created migrations

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-build:
-	@go build -o bin/gobank
+include .env
+export 
+
+build:	
+	@go build -o bin/gobank ./cmd/main.go
 
 run: build
 	@./bin/gobank
 
 init_postgres:
 	@docker-compose up --build -d
+
+up_migrations: 
+	@cd storage/migrations ; \
+	goose postgres "host=${POSTGRESDB_HOST} user=${POSTGRESDB_USER} database=${POSTGRESDB_NAME} password=${POSTGRESDB_PASSWORD} sslmode=${POSTGRESDB_SSLMODE}" up
+
---
+++ storage/migrations/20230920145801_create_articles.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS articles (
+  id SERIAL PRIMARY KEY,
+  source_id INT NOT NULL,
+  title VARCHAR(255)  NOT NULL,
+  link VARCHAR(255) NOT NULL,
+  summary TEXT NOT NULL,
+  published_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  posted_at TIMESTAMP
+);
+-- +goose StatementEnd
+
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
